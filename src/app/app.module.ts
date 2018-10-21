2bbb9f612997477f90a343663d7fb3e24be0fe50
Dell <farros@rambler.ru>
2018-10-21 12:30:50 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+import {RouterModule} from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+//----------general---------------------------------------
+import { GeneralModule} from './general/general.module';
+//--------------------material--------------------------
+import { MaterialModule} from './material/material.module';
+import {FlexLayoutModule } from '@angular/flex-layout';
+//------------------services------------------------------
+import {CommunicationService} from './services/AppService';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+        BrowserAnimationsModule,
+        HttpClientModule,
+        RouterModule,
+        BrowserModule.withServerTransition({ appId: 'serverApp' }),
+        //-------------material----------------------------
+        MaterialModule,
+        FlexLayoutModule,
+        //-------------------------------------------------
+        GeneralModule,
+        AppRoutingModule,
+
   ],
-  providers: [],
+  providers: [CommunicationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
